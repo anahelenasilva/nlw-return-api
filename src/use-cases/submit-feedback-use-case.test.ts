2bd5520a264a915374012c5058351344a6cae8b2
Ana Helena <anahelenarp@hotmail.com>
2022-05-05 19:30:31 -0300

    test: add spies to test

--- src/use-cases/submit-feedback-use-case.test.ts
+++ src/use-cases/submit-feedback-use-case.test.ts
@@ -1,8 +1,10 @@
 import SubmitFeedbackUseCase from "./submit-feedback-use-case"
 
+const createFeedbackSpy = jest.fn()
+const sendMailSpy = jest.fn()
 const sut = new SubmitFeedbackUseCase(
-    { create: async () => { } },
-    { sendMail: async () => { } },
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy },
 )
 
 describe('Submit Feedback UseCase', () => {
@@ -12,6 +14,9 @@ describe('Submit Feedback UseCase', () => {
             type: 'BUG',
             screenshot: 'data:image/png;base64,798ds7f987d987f98d'
         })).resolves.not.toThrow()
+
+        expect(createFeedbackSpy).toHaveBeenCalled()
+        expect(sendMailSpy).toHaveBeenCalled()
     })
     it('should not be able to submit a feedback without type', async () => {
         await expect(sut.execute({
